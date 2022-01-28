d6a20b49374a0efa0679b6e5743b6776a7df679f
Aimlclubcmrit <98299719+Aimlclubcmrit@users.noreply.github.com>
2022-01-28 14:04:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def pyt():
             user='not'
             return redirect(url_for('success', name = user))
         else:
-            return render_template('pyt.html',result='/im/'+user,res=user+' If plugin not supported click on this to download your certificate')
+            return render_template('pyt.html',result='/im/'+user,res=user+' If plugin is not supporting click on this to download your certificate')
     else:
         return render_template('pyt.html')
 
